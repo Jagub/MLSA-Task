b488bcdd1f94c52d03329f1df968e1af242f38c9
Jagjot singh <0211ece095@niet.co.in>
2024-01-01 12:40:33 +0530

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Jagjot Singh",
+  title: "Ai-Ml Engineer and Web Developer",
+  email: "jagjotofficial1712@gmail.com",
+  gitHub: "jagub",
+  instagram: "",
+  linkedIn: "jagjotsingh",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/sailboats.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "sailboat image ";
 
 /**
  * Project list
@@ -31,29 +31,24 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My Blog Website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This is my blog website where I write about my experiences and learnings in the tech industry.",
+    url: "https://jagub.github.io/AI-ML-Project1-AWS-Udacity-Scholarship-/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Web Development project",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "mMy First web development project",
+    url: "https://jagub.github.io/Sparks-foundation-project-task-1/",
   },
   {
-    title: "My Resume Site",
+    title: "My Linkedin",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Connect with  me Here",
+    url: "https://www.linkedin.com/in/jagjot-singh-b07445227/",
   },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+  
 ];
 
 const Portfolio = () => {
